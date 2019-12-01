f97882de9595918c7451c7164fe4190ce8c352b3
DaynoniX <daynonixx@gmail.com>
2019-12-01 22:37:54 +0200

    THI IS COMMIT, MOTHERHUGGER!!!

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,22 +3,3 @@
 //= ../../../bower_components/slick-carousel/slick/slick.js
 
 
-$(document).ready(function(){
-    $('#mainSlider').slick();
-
-
-    function toggle_menu(trigger, block) {
-        $(trigger).click(function () {
-            $(block).toggleClass('active')
-        });
-    }
-
-    $('.sidebar-block_heading').click(function(){
-        $(this).closest('.sidebar-block').toggleClass('active')
-    });
-
-
-
-    toggle_menu('#filter_trigger', '.sidebar');
-    toggle_menu('.toggle', '.header')
-});
